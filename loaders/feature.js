74a8f50bb0db29149bf4a41fd94a4c26124d0b27
Trevor Scheer <tscheer@wayfair.com>
2018-02-20 21:24:26 -0500

    Add support and demo for amd modules

--- loaders/feature.js
+++ loaders/feature.js
@@ -1,13 +1,16 @@
+// Using module.exports down below to support AMD + ES6 modules simultaneously.
+// ES6 default export results in an object w/a default key when importing to AMD
+// {default: true/false}
 module.exports = function(source, map) {
   if (this.resourceQuery) {
     const name = this.resourceQuery.slice().replace('?', '');
     return `
       let _global = typeof window === 'undefined' ? global : window;
-      export default _global.wf ? _global.wf.appData.features.${name} : false;
+      module.exports = _global.wf ? _global.wf.appData.features.${name} : false;
     `;
   } else {
     return `
-      export default false;
+      module.exports = false;
     `;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import wf from './wf';
 
 import feature1 from '@feature/feature1';
-import feature2 from '@feature/feature2';
-import feature3 from 'feature!?feature3';
-import feature4 from 'feature!?feature4';
+import feature2 from 'feature!?feature2';
+import amdModuleSadness from './amd-module';
 
 console.log(feature1);
 console.log(feature2);
-console.log(feature3);
-console.log(feature4);
